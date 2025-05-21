dabc7d6ee9be381eaf1d9dae740e60d4e92fc18a
Sv3r <85077883+Sv3r@users.noreply.github.com>
2025-05-21 16:07:50 +0200

    feat: add go lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,9 +16,10 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            lspconfig.lua_ls.setup({capabilities = capabilities})
-            lspconfig.clangd.setup({capabilities = capabilities})
-            lspconfig.pyright.setup({capabilities = capabilities})
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.clangd.setup({ capabilities = capabilities })
+            lspconfig.pyright.setup({ capabilities = capabilities })
+            lspconfig.gopls.setup({ capabilities = capabilities })
 
             vim.keymap.set("n", "<leader>mp", vim.lsp.buf.format)
             vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
@@ -26,6 +27,25 @@ return {
             vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references)
             vim.keymap.set("n", "<leader>ri", vim.lsp.buf.implementation)
             vim.keymap.set("n", "<leader>O", vim.lsp.buf.document_symbol)
+
+            -- Formattin on save for go
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                    local params = vim.lsp.util.make_range_params()
+                    params.context = { only = { "source.organizeImports" } }
+                    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                    for cid, res in pairs(result or {}) do
+                        for _, r in pairs(res.result or {}) do
+                            if r.edit then
+                                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                            end
+                        end
+                    end
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
         end,
     }
 }
