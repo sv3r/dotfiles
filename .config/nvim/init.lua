d58d39dda3396ae2ebd4b89317686afc081ba34a
sv3r <85077883+Sv3r@users.noreply.github.com>
2025-08-14 21:56:12 +0200

    chore: reorganize nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,2 @@
 require("options")
 require("keymaps")
-
--- Packages
-vim.pack.add({
-	{src = "https://github.com/bluz71/vim-moonfly-colors"}
-})
-
--- Theme
-vim.cmd.colorscheme("moonfly")
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,259 +1,110 @@
--- Line
-vim.opt.number = true                               
-vim.opt.relativenumber = true                       
-vim.opt.cursorline = true                           
-vim.opt.wrap = false                                
-vim.opt.scrolloff = 10                              
-vim.opt.sidescrolloff = 8                           
+local undodir = vim.fn.expand("~/.vim/undodir")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+
+-- Scrolloff
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
 
 -- Indentation
-vim.opt.tabstop = 4                                 
-vim.opt.shiftwidth = 4                              
-vim.opt.softtabstop = 4                             
-vim.opt.expandtab = true                            
-vim.opt.smartindent = true                          
-vim.opt.autoindent = true                           
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- Search
-vim.opt.ignorecase = true                           
-vim.opt.smartcase = true                             
-vim.opt.hlsearch = false                            
-vim.opt.incsearch = true                            
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 -- Visual
-vim.opt.termguicolors = true                        
-vim.opt.signcolumn = "yes"                          
-vim.opt.showmatch = true                           
-vim.opt.completeopt = "menuone,noinsert,noselect"   
-vim.opt.pumheight = 10                             
-vim.opt.pumblend = 10                             
-vim.opt.winblend = 0                             
-vim.opt.conceallevel = 0                         
-vim.opt.concealcursor = ""                      
-vim.opt.lazyredraw = true
-vim.opt.synmaxcol = 300                          
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.guicursor = {
+    "n-v-c:block",
+    "i-ci-ve:block",
+    "r-cr:hor20",
+    "o:hor50"
+}
 
--- File
-vim.opt.backup = false                              
-vim.opt.writebackup = false                         
-vim.opt.swapfile = false                            
-vim.opt.undofile = true                             
-vim.opt.undodir = vim.fn.expand("~/.vim/undodir/")  
-vim.opt.updatetime = 300                            
-vim.opt.timeoutlen = 500                            
-vim.opt.ttimeoutlen = 0                             
-vim.opt.autoread = true                             
-vim.opt.autowrite = false                          
+-- Brackets
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+
+-- Files
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- Behaviour
-vim.opt.hidden = true                               
-vim.opt.errorbells = false                          
-vim.opt.backspace = "indent,eol,start"             
-vim.opt.autochdir = false                         
-vim.opt.iskeyword:append("-")                    
-vim.opt.path:append("**")                       
-vim.opt.selection = "exclusive"                     
-vim.opt.mouse = "a"                                 
-vim.opt.clipboard:append("unnamedplus")             
-vim.opt.modifiable = true                      
-vim.opt.encoding = "UTF-8"                          
+vim.opt.diffopt:append("linematch:60")
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.iskeyword:append("-")
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 0
 
--- Cursor
-vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+-- Performance
+vim.opt.redrawtime = 10000
+vim.opt.maxmempattern = 20000
 
--- Folding settings
-vim.opt.foldmethod = "expr"                       
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"  
-vim.opt.foldlevel = 99                          
+-- Blasphemies 
+vim.opt.mouse = "a"
 
--- Split behaviour
-vim.opt.splitbelow = true                      
-vim.opt.splitright = true                     
-
--- Key mappings
-vim.g.mapleader = " "                        
-vim.g.maplocalleader = " "                  
-
--- Copy Full File-Path
-vim.keymap.set("n", "<leader>pa", function()
-	local path = vim.fn.expand("%:p")
-	vim.fn.setreg("+", path)
-	print("file:", path)
-end)
-
--- Basic autocommands
 local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    group = augroup,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- Auto-close terminal when process exits
-vim.api.nvim_create_autocmd("TermClose", {
-  group = augroup,
-  callback = function()
-    if vim.v.event.status == 0 then
-      vim.api.nvim_buf_delete(0, {})
-    end
-  end,
-})
-
--- Disable line numbers in terminal
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = augroup,
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = "no"
-  end,
+    group = augroup,
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
 })
 
 -- Auto-resize splits when window is resized
 vim.api.nvim_create_autocmd("VimResized", {
-  group = augroup,
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
+    group = augroup,
+    callback = function()
+        vim.cmd("tabdo wincmd =")
+    end,
 })
 
 -- Create directories when saving files
 vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  callback = function()
-    local dir = vim.fn.expand('<afile>:p:h')
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, 'p')
-    end
-  end,
+    group = augroup,
+    callback = function()
+        local dir = vim.fn.expand("<afile>:p:h")
+        if vim.fn.isdirectory(dir) == 0 then
+            vim.fn.mkdir(dir, "p")
+        end
+    end,
 })
 
--- Command-line completion
-vim.opt.wildmenu = true
-vim.opt.wildmode = "longest:full,full"
-vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
-
--- Better diff options
-vim.opt.diffopt:append("linematch:60")
-
--- Performance improvements
-vim.opt.redrawtime = 10000
-vim.opt.maxmempattern = 20000
-
 -- Create undo directory if it doesn't exist
-local undodir = vim.fn.expand("~/.vim/undodir")
 if vim.fn.isdirectory(undodir) == 0 then
-  vim.fn.mkdir(undodir, "p")
+    vim.fn.mkdir(undodir, "p")
 end
-
--- terminal
-local terminal_state = {
-  buf = nil,
-  win = nil,
-  is_open = false
-}
-
-local function FloatingTerminal()
-  -- If terminal is already open, close it (toggle behavior)
-  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
-    vim.api.nvim_win_close(terminal_state.win, false)
-    terminal_state.is_open = false
-    return
-  end
-
-  -- Create buffer if it doesn't exist or is invalid
-  if not terminal_state.buf or not vim.api.nvim_buf_is_valid(terminal_state.buf) then
-    terminal_state.buf = vim.api.nvim_create_buf(false, true)
-    -- Set buffer options for better terminal experience
-    vim.api.nvim_buf_set_option(terminal_state.buf, 'bufhidden', 'hide')
-  end
-
-  -- Calculate window dimensions
-  local width = math.floor(vim.o.columns * 0.8)
-  local height = math.floor(vim.o.lines * 0.8)
-  local row = math.floor((vim.o.lines - height) / 2)
-  local col = math.floor((vim.o.columns - width) / 2)
-
-  -- Create the floating window
-  terminal_state.win = vim.api.nvim_open_win(terminal_state.buf, true, {
-    relative = 'editor',
-    width = width,
-    height = height,
-    row = row,
-    col = col,
-    style = 'minimal',
-    border = 'rounded',
-  })
-
-  -- Set transparency for the floating window
-  vim.api.nvim_win_set_option(terminal_state.win, 'winblend', 0)
-
-  -- Set transparent background for the window
-  vim.api.nvim_win_set_option(terminal_state.win, 'winhighlight',
-    'Normal:FloatingTermNormal,FloatBorder:FloatingTermBorder')
-
-  -- Define highlight groups for transparency
-  vim.api.nvim_set_hl(0, "FloatingTermNormal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "FloatingTermBorder", { bg = "none", })
-
-  -- Start terminal if not already running
-  local has_terminal = false
-  local lines = vim.api.nvim_buf_get_lines(terminal_state.buf, 0, -1, false)
-  for _, line in ipairs(lines) do
-    if line ~= "" then
-      has_terminal = true
-      break
-    end
-  end
-
-  if not has_terminal then
-    vim.fn.termopen(os.getenv("SHELL"))
-  end
-
-  terminal_state.is_open = true
-  vim.cmd("startinsert")
-
-  -- Set up auto-close on buffer leave 
-  vim.api.nvim_create_autocmd("BufLeave", {
-    buffer = terminal_state.buf,
-    callback = function()
-      if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
-        vim.api.nvim_win_close(terminal_state.win, false)
-        terminal_state.is_open = false
-      end
-    end,
-    once = true
-  })
-end
-
--- Function to explicitly close the terminal
-local function CloseFloatingTerminal()
-  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
-    vim.api.nvim_win_close(terminal_state.win, false)
-    terminal_state.is_open = false
-  end
-end
-
--- Key mappings
-vim.keymap.set("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "Toggle floating terminal" })
-vim.keymap.set("t", "<Esc>", function()
-  if terminal_state.is_open then
-    vim.api.nvim_win_close(terminal_state.win, false)
-    terminal_state.is_open = false
-  end
-end, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
