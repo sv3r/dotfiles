104e21ccd04a5524d16b0f9191b7559504cc59e3
sv3r <infosv3r@gmail.com>
2025-10-12 15:38:11 +0200

    fix: remove unused neovim keymap

--- .config/nvim/lua/plugins/moonfly.lua
+++ .config/nvim/lua/plugins/moonfly.lua
@@ -1,24 +1,5 @@
 vim.pack.add({ 'https://github.com/bluz71/vim-moonfly-colors' })
 
-local bg_transparent = false
-vim.g.moonflyTransparent = bg_transparent
-
-local reload_moonfly = function()
-    package.loaded['moonfly'] = nil
-    require('moonfly').style()
-    vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
-end
-
-reload_moonfly()
-
-local toggle_transparency = function()
-    bg_transparent = not bg_transparent
-    vim.g.moonflyTransparent = bg_transparent
-    reload_moonfly()
-end
-
-vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
 
 vim.cmd('colorscheme moonfly')
 
