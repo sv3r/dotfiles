66e048e3d78b8979d07b7aaee2d992504ecdbddf
sv3r <infosv3r@gmail.com>
2025-10-14 13:57:37 +0200

    feat: change shell to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if test -d $HOME/.local/bin
+    fish_add_path $HOME/.local/bin
+end
+
+set -g fish_greeting
---
+++ .config/fish/interactive.fish
@@ -0,0 +1,5 @@
+status --is-interactive; or exit
+
+if type -q rbenv
+    rbenv init - fish | source
+end
